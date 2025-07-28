d868f19381d6234d5da4ae980c6b35a9e7e40c07
Pratik Chaubey <pratikup98@gmail.com>
2025-07-28 21:11:16 +0530

    user detial

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
-const express = require('express');
+// // const express = require('express');
 
-// Create an instance of express
+// // // Create an instance of express
+// // const app = express();
+
+// // app.listen(5000, () => {
+// //   console.log('Server is running on port 4000');
+// // });
+
+// // app.get('/', (req, res) => {
+// //   res.send('Hello World! This is the home page.');
+// // });
+
+// // app.use(express.json());
+
+// // app.post('/api/car', (request, response) => {
+// //   const { car, price } = request.body;
+// //   console.log(`Car: ${car}, Price: ${price}`);
+// //   response.send('Data received successfully!');
+// // });
+
+const  express = require('express');
+
 const app = express();
 
-app.listen(5000, () => {
-  console.log('Server is running on port 4000');
+app.listen(6000, () => {
+    console.log('Server is running on port 6000');
 });
 
 app.get('/', (req, res) => {
@@ -13,8 +33,9 @@ app.get('/', (req, res) => {
 
 app.use(express.json());
 
-app.post('/api/car', (request, response) => {
-  const { car, price } = request.body;
-  console.log(`Car: ${car}, Price: ${price}`);
+app.post('/api/userdetial', (request, response) => {
+  const { name, age } = request.body;
+  console.log(`Name: ${name}, Age: ${age}`);
   response.send('Data received successfully!');
 });
+
