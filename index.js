81ca927c3876044a52cb8c52166584d05a6604d8
Pratik Chaubey <pratikup98@gmail.com>
2025-07-28 20:43:29 +0530

    api call

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 // Create an instance of express
 const app = express();
 
-app.listen(4000, () => {
+app.listen(5000, () => {
   console.log('Server is running on port 4000');
 });
 
