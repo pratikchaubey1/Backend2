b7d1263d9d491cfd09e104bd5eb0a33e2a6355cf
Pratik Chaubey <pratikup98@gmail.com>
2025-08-02 22:28:16 +0530

    api:

--- index.js
+++ index.js
@@ -19,44 +19,44 @@
 // // //   response.send('Data received successfully!');
 // // // });
 
-// // const  express = require('express');
-
-// // const app = express();
-
-// // app.listen(6000, () => {
-// //     console.log('Server is running on port 6000');
-// // });
-
-// // app.get('/', (req, res) => {
-// //   res.send('Hello World! This is the home page.');
-// // });
-
-// // app.use(express.json());
-
-// // app.post('/api/userdetial', (request, response) => {
-// //   const { name, age } = request.body;
-// //   console.log(`Name: ${name}, Age: ${age}`);
-// //   response.send('Data received successfully!');
-// // });
-
-// const express = require('express')
+// const  express = require('express');
 
 // const app = express();
 
 // app.listen(6000, () => {
-//   console.log('server is running on port 6000');
-  
+//     console.log('Server is running on port 6000');
 // });
 
-// app.get('/',(req,res)=>{
-//   res.send('hello world! this is the home page ')
+// app.get('/', (req, res) => {
+//   res.send('Hello World! This is the home page.');
 // });
 
 // app.use(express.json());
 
-// app.use('/api/login',(req,res)=>{
-//   const{email,password}= req.body;
-//   console.log(`email: ${email}, password: ${password}`);
-//   res.send('Data received successfully')
+// app.post('/api/userdetial', (request, response) => {
+//   const { name, age } = request.body;
+//   console.log(`Name: ${name}, Age: ${age}`);
+//   response.send('Data received successfully!');
 // });
+
+const express = require('express')
+
+const app = express();
+
+app.listen(6000, () => {
+  console.log('server is running on port 6000');
+  
+});
+
+app.get('/',(req,res)=>{
+  res.send('hello world! this is the home page ')
+});
+
+app.use(express.json());
+
+app.use('/api/login',(req,res)=>{
+  const{email,password}= req.body;
+  console.log(`email: ${email}, password: ${password}`);
+  res.send('Data received successfully')
+});
 
